32c6455025cd72a61993dd9e1883ab6e7a16dbde
Frank Morgner <frankmorgner@gmail.com>
2022-01-17 17:51:17 +0100

    a

--- .github/build.sh
+++ .github/build.sh
@@ -7,37 +7,42 @@ set -ex -o xtrace
 DEPS="gengetopt help2man automake libtool"
 
 case "$1" in
+    ubuntu|coverity)
+        DEPS="$DEPS gccgo golang-go openjdk-8-jdk openjdk-8-jre-headless python-dev ruby-dev swig xutils-dev doxygen"
+        ;;
     mingw-32)
         DEPS="$DEPS mingw-w64-tools binutils-mingw-w64-i686 gcc-mingw-w64-i686"
         ;;
     mingw-64)
         DEPS="$DEPS mingw-w64-tools binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64"
         ;;
-    ubuntu|coverity)
-        DEPS="$DEPS gccgo golang-go openjdk-8-jdk openjdk-8-jre-headless python-dev ruby-dev swig xutils-dev doxygen"
+esac
+
+case "$1" in
+    ubuntu|coverity|mingw-32|mingw-64)
+        sudo apt-get install -y $DEPS
+        ;;
+    macos)
+        brew install $DEPS
         ;;
 esac
 
-if [ "$1" == "macos" ]; then
-    brew install $DEPS
-else
-    sudo apt-get install -y $DEPS
-fi
-
-if [ "$1" == "ubuntu" ]; then
-    # full documentation is only built on ubuntu
-    pip install -U sphinx sphinx-bootstrap-theme breathe sphinxcontrib-programoutput
-fi
+case "$1" in
+    ubuntu)
+        # full documentation is only built on ubuntu
+        pip install -U sphinx sphinx-bootstrap-theme breathe sphinxcontrib-programoutput
+        ;;
+esac
 
 autoreconf -vis
 
 case "$1" in
-    mingw-32|mingw-64|macos)
-        ./configure --enable-openssl-install
-        ;;
     ubuntu|coverity)
         export GCCGOFLAGS="-static-libgcc $CFLAGS"
         ./configure --enable-python --enable-java --enable-ruby --enable-go
+        ;;
+    mingw-32|mingw-64|macos)
+        ./configure --enable-openssl-install
         ;;
 esac
 
